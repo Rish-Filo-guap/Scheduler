5061a6dc0047e8af45bf37007575d069c9edd32c
Rish-Filo-guap <164545101+Rish-Filo-guap@users.noreply.github.com>
2025-02-14 16:55:44 +0300

    делим на фрагменты

---
+++ app/src/main/java/com/example/scheduler/FirstPageViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.scheduler
+
+import androidx.lifecycle.ViewModel
+
+class FirstPageViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/scheduler/ui/main/ViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.scheduler.ui.main
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    private val fragments = mutableListOf<Fragment>()
+    private val fragmentTitles = mutableListOf<String>()
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragments.add(fragment)
+        fragmentTitles.add(title)
+    }
+
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+
+    fun getPageTitle(position: Int): String {
+        return fragmentTitles[position]
+    }
+}
