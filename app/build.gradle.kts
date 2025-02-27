c1b8e6768caa1363359ec59ff2d922f2d8445e47
Rish-Filo-guap <164545101+Rish-Filo-guap@users.noreply.github.com>
2025-02-27 03:43:53 +0300

    номер версии

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 24
         //noinspection ExpiredTargetSdkVersion
         targetSdk = 34
-        versionCode = 3
-        versionName = "2.1"
+        versionCode = 4
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
